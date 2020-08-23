f2a650ac7ae27596d38e9f882a0b9cb15813aef1
Ramya Challa <ramyac6@cs.washington.edu>
2020-08-23 01:24:48 -0700

    if you can sign up, go to login

--- signup.js
+++ signup.js
@@ -1,28 +1,30 @@
-import firebase from './firebase.js';
-var link = document.getElementById('signUp');
-link.addEventListener('click', signUp);
+import firebase from "./firebase.js";
+var link = document.getElementById("signUp");
+link.addEventListener("click", signUp);
 
-function signUp(){
-    var email = document.getElementById("email").value
-    var password = document.getElementById("password").value
-    var name = document.getElementById("name").value
-    localStorage.setItem("drMedName", name)
-    console.log(email)
-    console.log(password)
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log("Success")
-        window.location.href = "patient-info.html"
+function signUp() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var name = document.getElementById("name").value;
+  localStorage.setItem("drMedName", name);
+  console.log(email);
+  console.log(password);
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("Success");
+      window.location.href = "patient-info.html";
 
-        if (errorCode != null || errorMessage != null){
-            alert("Error: " + errorCode + errorMessage)
-        } else {
-            alert("You registered Succesfully!")
-            window.location.href = "patient-info.html"
-        }
-        window.location.href = "patient-info.html"
-        // ...
+      if (errorCode != null || errorMessage != null) {
+        alert("Error: " + errorCode + errorMessage);
+      } else {
+        alert("You registered Succesfully!");
+        window.location.href = "login.html";
+      }
+      // ...
     });
 }

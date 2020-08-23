5f6ae6bc01dfbd54e34b5781d30550b861f6addf
Ramya Challa <ramyac6@cs.washington.edu>
2020-08-22 22:36:09 -0700

    updated firebase stuff

--- firebase.js
+++ firebase.js
@@ -1,17 +1,17 @@
 const config = {
-    apiKey: "AIzaSyAEpK-QycuWRN2QAVX9OxYtoQh5aOYzD7I",
-    authDomain: "sigmahacks-f60d1.firebaseapp.com",
-    databaseURL: "https://sigmahacks-f60d1.firebaseio.com",
-    projectId: "sigmahacks-f60d1",
-    storageBucket: "sigmahacks-f60d1.appspot.com",
-    messagingSenderId: "192614888023",
-    appId: "1:192614888023:web:11e0706d66cd5e3786a912",
-    measurementId: "G-7FB9PE0NZQ"
-  };
+  apiKey: "AIzaSyCUDrErgQSWUsIrEGTpwcLHAxRVo1GEpQw",
+  authDomain: "gohealth-287305.firebaseapp.com",
+  databaseURL: "https://gohealth-287305.firebaseio.com",
+  projectId: "gohealth-287305",
+  storageBucket: "gohealth-287305.appspot.com",
+  messagingSenderId: "193336133177",
+  appId: "1:193336133177:web:975c8c52c05ab32c2eefe8",
+  measurementId: "G-MN17SZD07V",
+};
 
-  if (!firebase.apps.length) {
-    firebase.initializeApp(config);
- }
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
 
-  export default firebase
-  export const firebaseAuth = firebase.auth
+export default firebase;
+export const firebaseAuth = firebase.auth;
